a7657b4b51ecf5f8b11f99c43a9b4e89477e1876
Gaven Sra <gsra5@my.bcit.ca>
2016-11-28 15:32:05 -0800

    second commit

--- mathgame.php
+++ mathgame.php
@@ -43,78 +43,78 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
 ?>
 
-        <!DOCTYPE HTML>
-        <html lang="en">
+<!DOCTYPE HTML>
+<html lang="en">
 
-        <head>
-            <title>Math Game</title>
-            <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
-            <meta charset="utf-8" />
-        </head>
+<head>
+    <title>Math Game</title>
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <meta charset="utf-8" />
+</head>
 
-        <body>
-            <div class="container">
-                <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>" method="post" role="form" class="form-horizontal">
-                    <div class="row">
-                        <div class="col-sm-4 col-sm-offset-4">
-                            <h1>Math Game</h1></div>
-                        <div class="col-sm-4"><a href="index.php" class="btn btn-default btn-sm">Logout</a></div>
-                    </div>
-                    <div class="row">
-                        <label class="col-sm-2 col-sm-offset-3">
-                            <?php echo $leftNum?>
-                        </label>
-                        <label class="col-sm-2">
-                            <?php echo $operator?>
-                        </label>
-                        <label class="col-sm-2">
-                            <?php echo $rightNum?>
-                        </label>
-                        <div class="col-sm-3"></div>
-                    </div>
-
-                    <input type="hidden" name="actualAnswer" value="<?php echo htmlspecialchars($actualAnswer);?>" />
-
-                    <div class="form-group">
-                        <div class="col-sm-3 col-sm-offset-4">
-                            <input type="text" class="form-control" id="answer" name="answer" value="<?php echo htmlspecialchars($answer);?>" placeholder="Enter answer" size="6">
-                        </div>
-                        <div class="col-sm-5"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-3 col-sm-offset-4">
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <button type="submit" class="btn btn-primary btn-sm">
-                                Submit</button>
-                        </div>
-                        <div class="col-sm-3"></div>
-                    </div>
-                </form>
-                <hr />
-                <div class="text-center">
-                    <p>
-                        <?php echo $message ?>
-                    </p>
-                    <p>Your answer:
-                        <?php echo $yourAnswer ?>
-                    </p>
-                    <p>Actual answer:
-                        <?php echo $theAnswer ?>
-                    </p>
-                </div>
-                <div class="row">
-                    <div class="col-sm-4 col-sm-offset-4">
-                    </div>
-                    <div class="col-sm-4"></div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-4 col-sm-offset-4">Score:
-                        <?php echo $correct?> /
-                            <?php echo $total?>
-                    </div>
-                    <div class="col-sm-4"></div>
-                </div>
+<body>
+    <div class="container">
+        <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>" method="post" role="form" class="form-horizontal">
+            <div class="row">
+                <div class="col-sm-4 col-sm-offset-4">
+                    <h1>Math Game</h1></div>
+                <div class="col-sm-4"><a href="index.php" class="btn btn-default btn-sm">Logout</a></div>
             </div>
-        </body>
+            <div class="row">
+                <label class="col-sm-2 col-sm-offset-3">
+                    <?php echo $leftNum?>
+                </label>
+                <label class="col-sm-2">
+                    <?php echo $operator?>
+                </label>
+                <label class="col-sm-2">
+                    <?php echo $rightNum?>
+                </label>
+                <div class="col-sm-3"></div>
+            </div>
 
-        </html>
+            <input type="hidden" name="actualAnswer" value="<?php echo htmlspecialchars($actualAnswer);?>" />
+
+            <div class="form-group">
+                <div class="col-sm-3 col-sm-offset-4">
+                    <input type="text" class="form-control" id="answer" name="answer" value="<?php echo htmlspecialchars($answer);?>" placeholder="Enter answer" size="6">
+                </div>
+                <div class="col-sm-5"></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3 col-sm-offset-4">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button type="submit" class="btn btn-primary btn-sm">
+                        Submit</button>
+                </div>
+                <div class="col-sm-3"></div>
+            </div>
+        </form>
+        <hr />
+        <div class="text-center">
+            <p>
+                <?php echo $message ?>
+            </p>
+            <p>Your answer:
+                <?php echo $yourAnswer ?>
+            </p>
+            <p>Actual answer:
+                <?php echo $theAnswer ?>
+            </p>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4">
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4">Score:
+                <?php echo $correct?> /
+                    <?php echo $total?>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+    </div>
+</body>
+
+</html>
